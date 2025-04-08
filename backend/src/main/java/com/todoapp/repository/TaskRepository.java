8f0b6a74b21b78a92441030a0644788e4718288b
Lucas Franca <lucascfranca11@gmail.com>
2025-04-07 23:31:57 -0300

    Removendo issues SonarQube

--- backend/src/main/java/com/todoapp/repository/TaskRepository.java
+++ backend/src/main/java/com/todoapp/repository/TaskRepository.java
@@ -2,9 +2,6 @@ package com.todoapp.repository;
 
 import com.todoapp.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
